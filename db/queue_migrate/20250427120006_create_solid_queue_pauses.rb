49b0905e786c80195821834a7d0b02b14569a574
vikas2580 <chaurasiavikas1998@gmail.com>
2025-05-02 19:06:08 +0530

    Delete cache_schema.rb and queue_schema.rb and change in gemfile and database.yml file

--- db/queue_migrate/20250427120006_create_solid_queue_pauses.rb
+++
@@ -1,9 +0,0 @@
-class CreateSolidQueuePauses < ActiveRecord::Migration[7.1]
-  def change
-    create_table :solid_queue_pauses, force: :cascade do |t|
-      t.string :queue_name, null: false
-      t.datetime :created_at, null: false
-      t.index [:queue_name], name: "index_solid_queue_pauses_on_queue_name", unique: true
-    end
-  end
-end
