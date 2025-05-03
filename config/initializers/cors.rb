181f36fb738dbe9763de30efaff98479ca20b436
yeshu11 <uyeshu99@gmail.com>
2025-05-03 23:46:14 +0530

    Update CORS & Enable http_authenticatable for AJAX in Devise & Configure session store for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,19 +1,10 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'http://localhost:3000' # Adjust to your frontend domain
-#     resource '*', 
-#       headers: :any, 
-#       methods: [:get, :post, :put, :patch, :delete, :options],
-#       credentials: true # Allows sending cookies/auth tokens
-#   end
-# end
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://genzdeveloper.com' # Allow local dev and production domain
-    resource '*', 
-      headers: :any, 
+    origins 'http://localhost:3000', 'https://genzdeveloper.com'
+    resource '*',
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options],
-      credentials: true # Allows sending cookies/auth tokens
+      credentials: true,
+      expose: ['Set-Cookie']
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_myapp_session', expire_after: 1.day
+Rails.application.config.session_store :cookie_store, key: '_myapp_session', secure: Rails.env.production?, same_site: :lax
